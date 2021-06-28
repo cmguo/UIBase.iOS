6c8263c4e5d0f6090cdc236d410ab5b35aa27ca6
guochunmao <guochunmao@100tal.com>
2021-06-28 19:57:14 +0800

    fix VersionNumber

---
+++ Library/UIBase/UIBase.h
@@ -0,0 +1,18 @@
+//
+//  UIBase.h
+//  UIBase
+//
+//  Created by 郭春茂 on 2021/2/5.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIBase.
+FOUNDATION_EXPORT double UIBaseVersionNumber;
+
+//! Project version string for UIBase.
+FOUNDATION_EXPORT const unsigned char UIBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIBase/PublicHeader.h>
+
+#import <UIKit/UIKit.h>
